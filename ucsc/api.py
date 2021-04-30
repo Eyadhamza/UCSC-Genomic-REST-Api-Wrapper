02278532307af28d5eafc72c663bcb18a8c10f60
EyadHamza <EyadHamza0@outlook.com>
2021-04-30 09:49:49 +0200

    removed duplication

--- ucsc/api.py
+++ ucsc/api.py
@@ -13,15 +13,8 @@ class Hub:
         # call to api.genome.ucsc.edu/list/hubGenomes?hubUrl=http://hgdownload.soe.ucsc.edu/hubs/mouseStrains/hub.txt
         pass
 
-    def getChromosomes(self, genome):
-        # api.genome.ucsc.edu/list/chromosomes?hubUrl=http://hgdownload.soe.ucsc.edu/hubs/mouseStrains/hub.txt;genome=CAST_EiJ
 
-        return []
 
-    def getTrackChromosomes(self, genome, track):
-        # api.genome.ucsc.edu/list/chromosomes?hubUrl=hubUrl=http://hgdownload.soe.ucsc.edu/hubs/mouseStrains/hub.txt;genome=CAST_EiJ;track=assembly
-
-        return []
 
 
 class Genome:
@@ -37,11 +30,6 @@ class Genome:
         #  api.genome.ucsc.edu/list/ucscGenomes
         return []
 
-    def getChromosomes(self):
-        # call to list chromosomes from UCSC database genome -
-        # api.genome.ucsc.edu/list/chromosomes?genome=hg38
-
-        return []
 
     def trackList(self):
         return []
@@ -53,14 +41,22 @@ class Track:
         # return the track as an object with all required attributes
         self.trackName = trackName
 
-    def getTrackChromosomes(self, genome):
-        # list chromosomes from specified track in UCSC database genome
-        # api.genome.ucsc.edu/list/chromosomes?genome=hg38;track=gold
-        return []
+
 
     def createShema(self, genome):
         # api.genome.ucsc.edu/list/schema?genome=hg38;track=knownGene
         return ''
+
+
+class Chromosome:
+    def __init__(self, genome =None, hub =None, track =None):
+        # fetch the track based on name
+        # return the track as an object with all required attributes
+        self.trackName = trackName
+
+    @staticmethod
+    def getChromosomes(genome =None, hub =None, track =None):
+        return []
 
 
 # returns list of available hubs names and url (can be overriding by parameter)
@@ -73,7 +69,6 @@ hub = Hub(hubName)  # an object has every attribute of hub
 # get all genomes from specified hub object
 hub.getHubGenomes()
 
-
 # get all genomes from all UCSC Database
 genomesList = Genome.getUCSCGenomes()
 
@@ -81,7 +76,6 @@ genomeName = 'CAST_EiJ'  # researcher will choose his desired genome based on na
 
 genome = Genome(genomeName)  # an object has every attribute of genome
 
-chromosomes = genome.getChromosomes()
 
 tracks = genome.trackList()
 
@@ -89,20 +83,27 @@ trackName = 'affyGnf1h'
 
 track = Track(trackName)  # an object has every attribute of track
 
-
 # list chromosomes from specified track in UCSC database genome
-trackChromosomes = track.getTrackChromosomes(genome)
 
 # list schema from specified track in UCSC database genome
 trackSchema = track.createShema(genome)
 
 # duplication needs to be removed of course ! WARNING
 # list chromosomes from assembly hub genome -
-chromosomes2 = hub.getChromosomes(genome)
-
 
 # list chromosomes from specified track in assembly hub genome -
-trackChromosomes2 = hub.getTrackChromosomes(genome, track)
 
+# only one function to get the list of chromosomes
 
+# list chromosomes from UCSC database genome then hub and track = None
+chromosomes1 = Chromosome.getChromosomes(genome)
 
+# list chromosomes from specified track in UCSC database genome
+chromosomes2 = Chromosome.getChromosomes(genome, track=track)
+
+# list chromosomes from assembly hub genome
+chromosomes3 = Chromosome.getChromosomes(genome, hub)
+
+# list chromosomes from specified track in assembly hub genome
+
+chromosomes4 = Chromosome.getChromosomes(genome, hub, track)
