0239a6a6e54c67a0beadbcd95e9bee4b3aebeb94
EyadHamza <EyadHamza0@outlook.com>
2021-05-02 12:56:35 +0200

    Sequence

--- ucsc/api.py
+++ ucsc/api.py
@@ -6,7 +6,6 @@ BASE_URL = 'http://api.genome.ucsc.edu'
 class Hub:  # yasmeen
 
     def __init__(self, **kwargs):
-
         self.shortLabel = kwargs.get('shortLabel')
         self.hubUrl = kwargs.get('hubUrl')
         self.longLabel = kwargs.get('longLabel')
@@ -14,7 +13,6 @@ class Hub:  # yasmeen
         self.dbCount = kwargs.get('dbCount')
         self.dbList = kwargs.get('dbList')
         self.descriptionUrl = kwargs.get('descriptionUrl')
-
 
     @staticmethod
     def get():
@@ -24,11 +22,9 @@ class Hub:  # yasmeen
             myList.append(Hub(**key))
         return myList
 
-
     def getGenomes(self):
         return Genome.get(self.hubUrl)
         # call to api.genome.ucsc.edu/list/hubGenomes?hubUrl=http://hgdownload.soe.ucsc.edu/hubs/mouseStrains/hub.txt
-
 
 
 class Genome:  # eyad
@@ -59,7 +55,7 @@ class Genome:  # eyad
     def get(hubUrl=None):
         URL = 'ucscGenomes' if hubUrl is None else 'hubGenomes'
 
-        response = requests.get(BASE_URL + f'/list/{URL}', {'hubUrl':hubUrl}).json()
+        response = requests.get(BASE_URL + f'/list/{URL}', {'hubUrl': hubUrl}).json()
         genomesList = []
         genomesResponse = 'ucscGenomes' if hubUrl is None else 'genomes'
 
@@ -79,14 +75,13 @@ class Genome:  # eyad
     def tracks(self):
         return Track.get(self.genomeName)
 
-
     def findTrack(self, trackName):
         return Track.find(self.genomeName, trackName)
 
     def findTrackBy(self, attributeName, trackName):
-        return Track.findBy(self.genomeName, attributeName,trackName)
+        return Track.findBy(self.genomeName, attributeName, trackName)
 
-    def isTrackExists(self,trackName):
+    def isTrackExists(self, trackName):
         return Track.exists(self.genomeName, trackName)
 
     @staticmethod
@@ -107,7 +102,7 @@ class Genome:  # eyad
 
 
 class Track:  # mazen
-    def __init__(self,trackName, **kwargs):
+    def __init__(self, trackName, **kwargs):
         # fetch the track based on name
         # return the track as an object with all required attributes
         self.trackName = trackName
@@ -176,8 +171,6 @@ class Track:  # mazen
                 return track
         raise Exception("can't find track, Track does not exist")
 
-
-
     def schema(self, genomeName):
         return TrackSchema.get(genomeName, self.trackName)
 
@@ -203,42 +196,40 @@ class Chromosome:  # salma
     def __init__(self, chromosomeName):
         self.chromosomeName = chromosomeName
 
-
     @staticmethod
     def get(hub=None, genome=None, track=None):
         URL = BASE_URL + '/list/chromosomes'
-        response = requests.get(URL, {'genome': genome, 'track': track,'hub':hub})
+        response = requests.get(URL, {'genome': genome, 'track': track, 'hub': hub})
         chromosomesList = []
 
         for key in response.json()['chromosomes']:
             chromosomesList.append(Chromosome(key))
         return chromosomesList
 
-    def exists(hub=None, genome=None, track=None):
-        for track in Track.get(genomeName):
-            if track.trackName == trackName:
+    @staticmethod
+    def exists(chromosomeName, hub=None, genome=None, track=None):
+        for chromosome in Chromosome.get(hub, genome, track):
+            if chromosome.chromosomeName == chromosomeName:
                 return True
 
         return False
 
     @staticmethod
-    def find(hub=None, genome=None, track=None):
-        for track in Track.get(genomeName):
-            if track.trackName == trackName:
-                return track
-        raise Exception("can't find track, Track does not exist")
+    def find(chromosomeName, hub=None, genome=None, track=None):
+        for chromosome in Chromosome.get(hub, genome, track):
+            if chromosome.chromosomeName == chromosomeName:
+                return chromosome
 
-    @staticmethod
-    def findBy(hub=None, genome=None, track=None):
-        for track in Track.get(genomeName):
-            if getattr(track, trackAttribute) == value:
-                print('track found')
-                return track
-        raise Exception("can't find track, Track does not exist")
-
+        raise Exception("can't find chromosome, Chromosome does not exist")
 
 
 class Sequence:  # sohaila
-    def __init__(self, genome, chromosome, hub=None, track=None, chromStart=None, chromEnd=None):
-        # must return the
-        self.dna = None
+    def init(self, genome, chrom, dna, hub=None, track=None, start=None, end=None):
+        self.end = end
+        self.start = start
+        self.track = track
+        self.hub = hub
+        self.chrom = chrom
+        self.dna = dna
+        self.genome = genome
+
