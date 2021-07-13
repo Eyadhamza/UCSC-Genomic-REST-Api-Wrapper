0d128e811cc2768b9803450e3b4388a078e55abf
EyadHamza <EyadHamza0@outlook.com>
2021-07-13 04:42:01 +0200

    running tests

--- ucsc/test_api.py
+++ ucsc/test_api.py
@@ -11,6 +11,24 @@ class MyTestCase(unittest.TestCase):
         with self.assertRaises(NotFoundException):
             Hub.find('Hello there')
 
+    def test_it_returns_a_hub_object_by_attribute_if_it_exists(self):
+        hub = Hub.findBy('name','ALFA Hub')
+        self.assertIsInstance(hub, Hub)
+
+        with self.assertRaises(NotFoundException):
+            Hub.findBy('name','Hello there')
+
+    def test_it_returns_all_hubs_as_lists(self):
+        hubs = Hub.get()
+        self.assertIsInstance(hubs, list)
+
+    def test_it_returns_all_genomes_in_a_hub_as_lists(self):
+        hub = Hub.findBy('name', 'ALFA Hub')
+        genomes = hub.genomes
+        self.assertIsInstance(genomes, list)
+
+
+
 
 
 if __name__ == '__main__':
